868dcd80d2999e3222aa46f6bf3d4196f8e132fe
Mattis <journet.mattis@gmail.com>
2023-03-13 20:41:53 +0100

    maj sqlite and Course models manytomanyfield

--- django_react_project/course/models.py
+++ django_react_project/course/models.py
@@ -7,7 +7,7 @@ class Course(models.Model):
     course_name = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     duration = models.FloatField(help_text="En heures", validators=[MinValueValidator(0)])
-    students = models.ManyToManyField(to=Student, null=True, blank=True, default=None)
+    students = models.ManyToManyField(to=Student, blank=True, default=None)
 
     def __str__(self):
         return f"{self.course_name} ({self.author})"
