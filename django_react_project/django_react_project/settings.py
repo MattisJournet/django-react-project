5630c998e86ed25d874a99f24c4b842fee95a450
Mattis <journet.mattis@gmail.com>
2023-03-13 20:42:54 +0100

    maj secretkey to null

--- django_react_project/django_react_project/settings.py
+++ django_react_project/django_react_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l-iftbi_r^zmy#v3p53r_5r+4p7mpxh=)$ep23=qk9z4fruboz'
+SECRET_KEY = 'null'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
